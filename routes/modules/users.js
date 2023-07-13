c8bebda9a085cc6b8fb0681f4781f78661438ec9
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-07-13 21:12:52 +0800

    feat: add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const User = require('../../models/user')
 
 const router = express.Router()
 
@@ -6,8 +7,36 @@ router.get('/login', (req, res) => {
     res.render('login')
 })
 
+
+
 router.get('/register', (req, res) => {
     res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    User.findOne({ email }).then(user => {
+        if (user) {
+            errors.push({ message: '這個 Email 已經被註冊過了。' })
+            return res.render('register', {
+                errors,
+                name,
+                email,
+                password,
+                confirmPassword
+            })
+        } else {
+            return User.create({
+                name,
+                email,
+                password
+            })
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+        }
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
